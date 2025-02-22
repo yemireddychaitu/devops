152015d84a509073516137192dc6b38ad9fe8327
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-02-22 23:07:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def main():
         return 'Welcom to python flask world from repo-4 nov-27'
-    if __name__ == '__main__':
+If __name__ == '__main__':
                   app.run(host='0.0.0.0', port=8080)
 
