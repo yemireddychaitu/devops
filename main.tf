2ab6e600e62a7dcee9fa0289ecbc1bd4c4bab425
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-02-25 19:55:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "gcs_bucket" {
+  name          = "bucket-from-github"
+  location      = "us-central1"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  public_access_prevention = "enforced"
+}
