4d268019b63d74cf06875b2403d2c01e10aa8062
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-03-03 21:25:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "gcs_bucket5" {
-  name          = "bucket-from-github5"
+resource "google_storage_bucket" "gcs_bucket6" {
+  name          = "bucket-from-github6"
   location      = "us-central1"
   force_destroy = true
   uniform_bucket_level_access = true
