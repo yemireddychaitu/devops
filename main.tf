22058f3e67dfc53eb7d3088a27468689e7288d3a
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-03-03 23:25:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ resource "google_storage_bucket" "gcs_bucket6" {
   public_access_prevention = "enforced"
 }
 
+resource "google_storage_bucket" "gcs_bucket7" {
+  name          = "bucket-from-github7"
+  location      = "us-central1"
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  public_access_prevention = "enforced"
+}
+
 
 
 
