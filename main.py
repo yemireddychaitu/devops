8ac92053ba22d0636450c25aa14709dd24d77dac
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-03-18 19:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-        return 'Welcom to python flask world from github'
+        return 'Welcom to python flask world from github:v2'
 if __name__ == '__main__':
                   app.run(host='0.0.0.0', port=8080)
 
